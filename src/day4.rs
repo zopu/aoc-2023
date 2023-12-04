b0fbdbfde5de3b47b460b1d1564a3f2bef9b879c
Mike Perrow <mike.perrow@gmail.com>
2023-12-04 12:00:47 +0000

    factors out common work between parts

--- src/day4.rs
+++ src/day4.rs
@@ -3,29 +3,22 @@ use std::collections::HashSet;
 use itertools::Itertools;
 
 pub fn run(input: &str) -> color_eyre::Result<(u32, u32)> {
-    Ok((part1(input)?, part2(input)?))
-}
-
-fn part1(input: &str) -> color_eyre::Result<u32> {
-    let sum: usize = input
-        .lines()
-        .map(|line| {
-            let matches = scratchcard_matches(line).unwrap();
-            if matches > 0 {
-                1 << (matches - 1)
-            } else {
-                0
-            }
-        })
-        .sum();
-    Ok(sum as u32)
-}
-
-fn part2(input: &str) -> color_eyre::Result<u32> {
     let matches: Vec<_> = input
         .lines()
         .map(|line| scratchcard_matches(line).unwrap())
         .collect();
+    Ok((part1(&matches)?, part2(&matches)?))
+}
+
+fn part1(matches: &[usize]) -> color_eyre::Result<u32> {
+    let sum: usize = matches
+        .iter()
+        .map(|m| if *m > 0 { 1 << (m - 1) } else { 0 })
+        .sum();
+    Ok(sum as u32)
+}
+
+fn part2(matches: &[usize]) -> color_eyre::Result<u32> {
     let mut cards = vec![1; matches.len()];
     for i in 0..cards.len() {
         for j in 1..(matches[i] + 1) {
@@ -62,28 +55,28 @@ mod tests {
     #[test]
     fn test_sample_part1() -> color_eyre::Result<()> {
         let input = std::fs::read_to_string("inputs/4/sample.txt")?;
-        assert_eq!(13, part1(&input)?);
+        assert_eq!(13, run(&input)?.0);
         Ok(())
     }
 
     #[test]
     fn test_part1() -> color_eyre::Result<()> {
         let input = std::fs::read_to_string("inputs/4/input.txt")?;
-        assert_eq!(17782, part1(&input)?);
+        assert_eq!(17782, run(&input)?.0);
         Ok(())
     }
 
     #[test]
     fn test_sample_part2() -> color_eyre::Result<()> {
         let input = std::fs::read_to_string("inputs/4/sample.txt")?;
-        assert_eq!(30, part2(&input)?);
+        assert_eq!(30, run(&input)?.1);
         Ok(())
     }
 
     #[test]
     fn test_part2() -> color_eyre::Result<()> {
         let input = std::fs::read_to_string("inputs/4/input.txt")?;
-        assert_eq!(8477787, part2(&input)?);
+        assert_eq!(8477787, run(&input)?.1);
         Ok(())
     }
 }
