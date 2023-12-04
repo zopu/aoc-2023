4d536124ca843a4dec4c0277d8ba30f74dd1fa5c
Mike Perrow <mike.perrow@gmail.com>
2023-12-04 18:57:05 +0000

    error handling cleanup for part 4

--- src/day4.rs
+++ src/day4.rs
@@ -1,6 +1,7 @@
 use std::collections::HashSet;
 
 use color_eyre::eyre::anyhow;
+use color_eyre::Result;
 use nom::multi::separated_list1;
 use nom::sequence::{preceded, separated_pair};
 use nom::{bytes::complete::tag, sequence::tuple};
@@ -10,22 +11,23 @@ use nom::{
 };
 use rayon::{iter::ParallelIterator, str::ParallelString};
 
-pub fn run(input: &str) -> color_eyre::Result<(u32, u32)> {
-    let matches: Vec<_> = input
+pub fn run(input: &str) -> Result<(u32, u32)> {
+    let matches = input
         .par_lines()
-        .map(|line| scratchcard_matches(line).unwrap())
-        .collect();
+        .map(scratchcard_matches)
+        .collect::<Result<Vec<_>, _>>()?;
+
     Ok((part1(&matches)?, part2(&matches)?))
 }
 
-fn part1(matches: &[usize]) -> color_eyre::Result<u32> {
+fn part1(matches: &[usize]) -> Result<u32> {
     Ok(matches
         .iter()
         .map(|m| if *m > 0 { 1 << (m - 1) } else { 0 })
         .sum())
 }
 
-fn part2(matches: &[usize]) -> color_eyre::Result<u32> {
+fn part2(matches: &[usize]) -> Result<u32> {
     let mut cards = vec![1; matches.len()];
     for i in 0..cards.len() {
         for j in 1..(matches[i] + 1) {
@@ -37,7 +39,7 @@ fn part2(matches: &[usize]) -> color_eyre::Result<u32> {
     Ok(cards.iter().sum())
 }
 
-fn parse_card(line: &str) -> color_eyre::Result<(Vec<u32>, Vec<u32>)> {
+fn parse_card(line: &str) -> Result<(Vec<u32>, Vec<u32>)> {
     let (_remaining, (l, r)) = preceded(
         tuple((tag("Card"), multispace0, u32, tag(":"), multispace0)),
         separated_pair(
@@ -50,7 +52,7 @@ fn parse_card(line: &str) -> color_eyre::Result<(Vec<u32>, Vec<u32>)> {
     Ok((l, r))
 }
 
-fn scratchcard_matches(card: &str) -> color_eyre::Result<usize> {
+fn scratchcard_matches(card: &str) -> Result<usize> {
     let (l, r) = parse_card(card)?;
     let winners: HashSet<u32> = HashSet::from_iter(l);
     Ok(r.iter().filter(|n| winners.contains(n)).count())
@@ -61,38 +63,37 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_parse_line() -> color_eyre::Result<()> {
+    fn test_parse_line() {
         let line = "Card   3:  4 45 78 42 29 92 16 90 93 30 | 97 90 75 40 43 65 92 83 41  4 47 35 29 80 68 87 30 71 98 42 95  7 76 69 88";
-        let (l, r) = parse_card(line)?;
+        let (l, r) = parse_card(line).unwrap();
         assert_eq!(10, l.len());
         assert_eq!(25, r.len());
         assert_eq!(90, r[1]);
-        Ok(())
     }
 
     #[test]
-    fn test_sample_part1() -> color_eyre::Result<()> {
+    fn test_sample_part1() -> Result<()> {
         let input = std::fs::read_to_string("inputs/4/sample.txt")?;
         assert_eq!(13, run(&input)?.0);
         Ok(())
     }
 
     #[test]
-    fn test_part1() -> color_eyre::Result<()> {
+    fn test_part1() -> Result<()> {
         let input = std::fs::read_to_string("inputs/4/input.txt")?;
         assert_eq!(17782, run(&input)?.0);
         Ok(())
     }
 
     #[test]
-    fn test_sample_part2() -> color_eyre::Result<()> {
+    fn test_sample_part2() -> Result<()> {
         let input = std::fs::read_to_string("inputs/4/sample.txt")?;
         assert_eq!(30, run(&input)?.1);
         Ok(())
     }
 
     #[test]
-    fn test_part2() -> color_eyre::Result<()> {
+    fn test_part2() -> Result<()> {
         let input = std::fs::read_to_string("inputs/4/input.txt")?;
         assert_eq!(8477787, run(&input)?.1);
         Ok(())
