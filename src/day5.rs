a024068bc4d8c92dbe0c841d87a41124c6f6e1a8
Mike Perrow <mike.perrow@gmail.com>
2023-12-05 15:29:02 +0000

    converts to u64 throughout, adds unit tests for real input, some cleanup

--- src/day5.rs
+++ src/day5.rs
@@ -4,7 +4,7 @@ use std::ops::Range;
 use color_eyre::eyre::anyhow;
 use color_eyre::Result;
 use nom::branch::alt;
-use nom::character::complete::{alpha1, multispace1, newline, u32};
+use nom::character::complete::{alpha1, multispace1, newline, u64};
 use nom::combinator::map;
 use nom::multi::{many0, separated_list0};
 use nom::sequence::tuple;
@@ -12,13 +12,10 @@ use nom::{bytes::complete::tag, multi::separated_list1, sequence::preceded, IRes
 
 pub fn run(input: &str) -> Result<(u64, u64)> {
     let (_, (seeds, mappings)) = parse(input).map_err(|e| anyhow!("Parse error: {}", e))?;
-    Ok((
-        part1(&seeds, &mappings)? as u64,
-        part2(&seeds, &mappings)? as u64,
-    ))
+    Ok((part1(&seeds, &mappings)?, part2(&seeds, &mappings)?))
 }
 
-fn part1(seeds: &[u32], mappings: &[Mapping]) -> Result<u32> {
+fn part1(seeds: &[u64], mappings: &[Mapping]) -> Result<u64> {
     let mut vals = Vec::from(seeds);
     for mapping in mappings {
         vals = vals.iter().map(|v| mapping.apply(*v)).collect();
@@ -26,9 +23,8 @@ fn part1(seeds: &[u32], mappings: &[Mapping]) -> Result<u32> {
     Ok(*vals.iter().min().unwrap())
 }
 
-fn part2(seeds: &[u32], mappings: &[Mapping]) -> Result<u32> {
-    // let mut vals = Vec::from(seeds);
-    let mut ranges: Vec<Range<u32>> = seeds
+fn part2(seeds: &[u64], mappings: &[Mapping]) -> Result<u64> {
+    let mut ranges: Vec<Range<u64>> = seeds
         .chunks(2)
         .map(|chunk| Range {
             start: chunk[0],
@@ -47,26 +43,26 @@ fn part2(seeds: &[u32], mappings: &[Mapping]) -> Result<u32> {
 
 #[derive(Debug)]
 struct Mapping {
-    maps: Vec<(u32, u32, u32)>,
+    maps: Vec<(u64, u64, u64)>,
 }
 
 impl Mapping {
-    pub fn apply(&self, n: u32) -> u32 {
+    pub fn apply(&self, n: u64) -> u64 {
         for map in &self.maps {
-            if n >= map.1 && (n as u64) < (map.1 as u64 + map.2 as u64) {
-                return (n as i64 + (map.0 as i64 - map.1 as i64)) as u32;
+            if n >= map.1 && n < map.1 + map.2 {
+                return (n as i64 + (map.0 as i64 - map.1 as i64)) as u64;
             }
         }
         n
     }
 
-    fn apply_submap(n: u32, map: &(u32, u32, u32)) -> u32 {
-        (n as i64 + (map.0 as i64 - map.1 as i64)) as u32
+    fn apply_submap(n: u64, map: &(u64, u64, u64)) -> u64 {
+        (n as i64 + (map.0 as i64 - map.1 as i64)) as u64
     }
 
-    pub fn apply_to_range(&self, range: &Range<u32>) -> Vec<Range<u32>> {
-        let mut output_ranges: Vec<Range<u32>> = Vec::new();
-        let mut intersections: Vec<Range<u32>> = Vec::new();
+    pub fn apply_to_range(&self, range: &Range<u64>) -> Vec<Range<u64>> {
+        let mut output_ranges: Vec<Range<u64>> = Vec::new();
+        let mut intersections: Vec<Range<u64>> = Vec::new();
         for map in &self.maps {
             let map_range = Range {
                 start: map.1,
@@ -86,7 +82,7 @@ impl Mapping {
     }
 }
 
-fn intersection(r1: &Range<u32>, r2: &Range<u32>) -> Option<Range<u32>> {
+fn intersection(r1: &Range<u64>, r2: &Range<u64>) -> Option<Range<u64>> {
     let start = max(r1.start, r2.start);
     let end = min(r1.end, r2.end);
     if start <= end {
@@ -96,11 +92,9 @@ fn intersection(r1: &Range<u32>, r2: &Range<u32>) -> Option<Range<u32>> {
     }
 }
 
-// Will return either 0, 1 or 2 ranges being the subsets of r not containing to_removed.
-fn remove_ranges_from_range(range: &Range<u32>, mut to_remove: Vec<Range<u32>>) -> Vec<Range<u32>> {
+fn remove_ranges_from_range(range: &Range<u64>, mut to_remove: Vec<Range<u64>>) -> Vec<Range<u64>> {
     to_remove.sort_by(|a, b| a.start.partial_cmp(&b.start).unwrap());
-    // println!("to_remove sorted: {:?}", to_remove);
-    let mut output_ranges: Vec<Range<u32>> = Vec::new();
+    let mut output_ranges: Vec<Range<u64>> = Vec::new();
     let mut pos = range.start;
     for r in to_remove.iter() {
         if r.start > pos {
@@ -130,15 +124,15 @@ fn parse_one_mapping(input: &str) -> IResult<&str, Mapping> {
     let (remaining, vec_tuples) = separated_list1(
         newline,
         map(
-            tuple((u32, tag(" "), u32, tag(" "), u32)),
+            tuple((u64, tag(" "), u64, tag(" "), u64)),
             |(a, _, b, _, c)| (a, b, c),
         ),
     )(remaining)?;
     Ok((remaining, Mapping { maps: vec_tuples }))
 }
 
-fn parse(input: &str) -> IResult<&str, (Vec<u32>, Vec<Mapping>)> {
-    let (remaining, seeds) = preceded(tag("seeds: "), separated_list1(tag(" "), u32))(input)?;
+fn parse(input: &str) -> IResult<&str, (Vec<u64>, Vec<Mapping>)> {
+    let (remaining, seeds) = preceded(tag("seeds: "), separated_list1(tag(" "), u64))(input)?;
     let (remaining, _) = many0(newline)(remaining)?;
     let (remaining, mappings) = separated_list0(multispace1, parse_one_mapping)(remaining)?;
     Ok((remaining, (seeds, mappings)))
@@ -149,10 +143,12 @@ mod tests {
     use color_eyre::eyre::anyhow;
 
     use super::*;
-    use crate::runner::test_sample;
+    use crate::runner::{test_input, test_sample};
 
     test_sample!(sample_part1, 5, Some(35), None);
     test_sample!(sample_part2, 5, None, Some(46));
+    test_input!(part1, 5, Some(251346198), None);
+    test_input!(part2, 5, None, Some(72263011));
 
     #[test]
     fn can_parse_sample() -> Result<()> {
