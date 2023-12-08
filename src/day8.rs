0df3928614468b1c1940bf5d4d5a3cd4f2b93aa6
Mike Perrow <mike.perrow@gmail.com>
2023-12-08 15:53:21 +0000

    part2. Not happy with this yet but it gets the right answer so checking in...

--- src/day8.rs
+++ src/day8.rs
@@ -1,13 +1,14 @@
-use std::collections::HashMap;
-
 use color_eyre::{eyre::anyhow, Result};
 use nom::{
     bytes::complete::tag,
-    character::complete::alpha1,
+    character::complete::alphanumeric1,
     character::complete::char,
     sequence::{delimited, separated_pair, tuple},
     IResult,
 };
+use num::Integer;
+use std::collections::hash_map::Entry::Vacant;
+use std::collections::{HashMap, HashSet};
 
 #[derive(Debug, PartialEq, Eq)]
 enum Direction {
@@ -15,32 +16,110 @@ enum Direction {
     R,
 }
 
+type Node = u16;
+type StepCount = u64;
+
 pub fn run(input: &str) -> Result<(u64, u64)> {
     let parsed = parse(input)?;
 
+    let p1_start = parsed.names_map["AAA"];
+    let p1_end = parsed.names_map["ZZZ"];
+    let p1 = part1(&parsed.directions, p1_start, p1_end, &parsed.nodes_map)?;
+    Ok((p1, part2(&parsed)?))
+}
+
+fn part1(
+    directions: &[Direction],
+    start: u16,
+    end: u16,
+    nodes_map: &HashMap<u16, (u16, u16)>,
+) -> Result<u64> {
     let mut count = 0;
-    let mut location = parsed.names_map["AAA"];
-    let end = parsed.names_map["ZZZ"];
+    let mut location = start;
     // println!("Names map: {:?}", names_map);
-    for d in parsed.directions.iter().cycle() {
+    for d in directions.iter().cycle() {
         // println!("Visiting location {}", location);
         if location == end {
             break;
         }
-        let routes = parsed.nodes_map[&location];
+        let routes = nodes_map[&location];
         location = match d {
             Direction::L => routes.0,
             Direction::R => routes.1,
         };
         count += 1;
     }
-    Ok((count, 0))
+    Ok(count)
+}
+
+fn part2(parsed: &ParseOutput) -> Result<u64> {
+    let locations: Vec<Node> = parsed.ghost_starts.iter().cloned().collect();
+    let ghost_moves: Vec<(StepCount, StepCount)> = locations
+        .iter()
+        .map(|ghost_location| {
+            let (offset, loop_size, _offsets) = find_loop_and_offsets_for_ends(
+                &parsed.directions,
+                *ghost_location,
+                &parsed.nodes_map,
+                &parsed.ghost_ends,
+            )?;
+            Ok((offset, loop_size))
+        })
+        .collect::<Result<Vec<(StepCount, StepCount)>>>()?;
+
+    // This *happens* to work on both the sample data and input data, but doesn't generally work.
+    // Should expand to using something like CRT rather than LCM.
+    let answer: u64 = ghost_moves
+        .iter()
+        .cloned()
+        .fold(1u64, |p, (a, _)| p.lcm(&a));
+    Ok(answer)
+}
+
+#[allow(clippy::type_complexity)]
+fn find_loop_and_offsets_for_ends(
+    directions: &[Direction],
+    start: u16,
+    nodes_map: &HashMap<Node, (Node, Node)>,
+    ends: &HashSet<Node>,
+) -> Result<(StepCount, StepCount, Vec<(Node, StepCount)>)> {
+    let mut count: u64 = 0;
+    let mut location = start;
+    let mut visited_ends: HashMap<(Node, usize), StepCount> = HashMap::new(); // usize is direction step
+
+    #[allow(clippy::explicit_counter_loop)]
+    for (direction_step, d) in directions.iter().enumerate().cycle() {
+        if ends.contains(&location) {
+            if let Vacant(e) = visited_ends.entry((location, direction_step)) {
+                e.insert(count);
+            } else {
+                let loop_size = count - visited_ends[&(location, direction_step)];
+                let offset = count - loop_size;
+                let offsets = visited_ends
+                    .iter()
+                    .map(|((node, _), step_count)| (*node, *step_count))
+                    .filter(|(_, step_count)| *step_count > offset)
+                    .collect();
+                return Ok((offset, loop_size, offsets));
+            }
+        }
+
+        let routes = nodes_map[&location];
+        location = match d {
+            Direction::L => routes.0,
+            Direction::R => routes.1,
+        };
+        count += 1;
+    }
+    Err(anyhow!("Couldn't find a loop!"))
 }
 
 struct ParseOutput<'a> {
     directions: Vec<Direction>,
     names_map: HashMap<&'a str, u16>,
     nodes_map: HashMap<u16, (u16, u16)>,
+    ghost_starts: HashSet<u16>,
+    ghost_ends: HashSet<u16>,
 }
 
 fn parse(input: &str) -> Result<ParseOutput> {
@@ -59,6 +138,8 @@ fn parse(input: &str) -> Result<ParseOutput> {
     let mut names_count = 0;
     let mut nodes_map = HashMap::new();
     let mut names_map = HashMap::new();
+    let mut ghost_starts = HashSet::new();
+    let mut ghost_ends = HashSet::new();
     for line in it {
         let (_, (a, b, c)) = parse_node(line).map_err(|e| anyhow!("Node parse error {:?}", e))?;
         for name in [a, b, c] {
@@ -69,21 +150,32 @@ fn parse(input: &str) -> Result<ParseOutput> {
         }
         let (source, l, r) = (names_map[a], names_map[b], names_map[c]);
         nodes_map.insert(source, (l, r));
+        match a.chars().nth(2) {
+            Some('A') => {
+                ghost_starts.insert(source);
+            }
+            Some('Z') => {
+                ghost_ends.insert(source);
+            }
+            _ => {}
+        }
     }
     Ok(ParseOutput {
         directions,
         names_map,
         nodes_map,
+        ghost_starts,
+        ghost_ends,
     })
 }
 
 pub fn parse_node(line: &str) -> IResult<&str, (&str, &str, &str)> {
     let (_remaining, (a, _, (b, c))) = tuple((
-        alpha1,
+        alphanumeric1,
         tag(" = "),
         delimited(
             char('('),
-            separated_pair(alpha1, tag(", "), alpha1),
+            separated_pair(alphanumeric1, tag(", "), alphanumeric1),
             char(')'),
         ),
     ))(line)?;
@@ -97,6 +189,15 @@ mod tests {
 
     test_sample!(sample_part1, 8, Some(6), None);
     test_input!(part1, 8, Some(15517), None);
+    test_input!(part2, 8, None, Some(14935034899483));
+
+    #[test]
+    fn sample_part2() -> color_eyre::Result<()> {
+        let input = std::fs::read_to_string("inputs/8/sample_part2.txt")?;
+        let parsed = parse(&input)?;
+        assert_eq!(6, super::part2(&parsed)?);
+        Ok(())
+    }
 
     #[test]
     fn parse_node_line() {
