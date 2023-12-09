6bef64dc445fde6fc2169b5302614822f5b40fed
Mike Perrow <mike.perrow@gmail.com>
2023-12-09 16:13:08 +0000

    day 9: perf: 2X by using rayon and reverse-iterating for p2 (instead of actually reversing the vec)

--- src/day9.rs
+++ src/day9.rs
@@ -1,20 +1,22 @@
 use color_eyre::{eyre::anyhow, Result};
 use nom::{bytes::complete::tag, character::complete::i32, multi::separated_list1, IResult};
+use rayon::{iter::ParallelIterator, str::ParallelString};
 
 pub fn run(input: &str) -> Result<(u64, u64)> {
     let (p1, p2) = input
-        .lines()
+        .par_lines()
         .map(|l| {
-            let (_, mut v) = parse_line(l)
+            let (_, v) = parse_line(l)
                 .map_err(|e| anyhow!("Parse error: {}", e))
                 .unwrap();
-            let p1 = find_next(&v);
-            v.reverse();
-            let p2 = find_next(&v);
+            let p1 = find_next(v.iter());
+            let p2 = find_next(v.iter().rev());
             (p1, p2)
         })
-        .reduce(|(p1sum, p2sum), (p1, p2)| (p1sum + p1, p2sum + p2))
-        .unwrap();
+        .reduce(
+            || (0, 0),
+            |(p1sum, p2sum), (p1, p2)| (p1sum + p1, p2sum + p2),
+        );
     Ok((p1 as u64, p2 as u64))
 }
 
@@ -22,14 +24,14 @@ fn parse_line(input: &str) -> IResult<&str, Vec<i32>> {
     separated_list1(tag(" "), i32)(input)
 }
 
-fn find_next(seq: &[i32]) -> i32 {
-    let next_diffs = |mut v: Vec<i32>, n| -> Vec<i32> {
+fn find_next<'a>(seq: impl Iterator<Item = &'a i32>) -> i32 {
+    let next_diffs = |mut v: Vec<i32>, n: &i32| -> Vec<i32> {
         if v.is_empty() {
-            v.push(n);
+            v.push(*n);
             return v;
         };
         let mut last_v_i = v[0];
-        v[0] = n;
+        v[0] = *n;
         for i in 1..v.len() {
             (last_v_i, v[i]) = (v[i], v[i - 1] - last_v_i);
         }
@@ -39,7 +41,7 @@ fn find_next(seq: &[i32]) -> i32 {
         }
         v
     };
-    let diffs = seq.iter().cloned().fold(Vec::with_capacity(20), next_diffs);
+    let diffs = seq.fold(Vec::with_capacity(20), next_diffs);
     diffs.iter().sum()
 }
 
