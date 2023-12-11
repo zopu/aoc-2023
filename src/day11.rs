e21441cf86bac88ea805fbae139b96537ee745f0
Mike Perrow <mike.perrow@gmail.com>
2023-12-11 11:32:38 +0000

    day 11: part 2

--- src/day11.rs
+++ src/day11.rs
@@ -9,7 +9,7 @@ struct Universe {
 }
 
 impl Universe {
-    fn expand(&self) -> Self {
+    fn expand(&self, expansion: usize) -> Self {
         let (mut rows, mut cols) = (vec![0; self.dimensions.1], vec![0; self.dimensions.0]);
         for (c, r) in &self.galaxies {
             rows[*r] = 1;
@@ -18,7 +18,7 @@ impl Universe {
         let mut expanded = 0;
         for (i, r) in rows.iter_mut().enumerate() {
             if *r == 0 {
-                expanded += 1;
+                expanded += expansion - 1;
             }
             *r = i + expanded;
         }
@@ -26,7 +26,7 @@ impl Universe {
         let mut expanded = 0;
         for (i, c) in cols.iter_mut().enumerate() {
             if *c == 0 {
-                expanded += 1;
+                expanded += expansion - 1;
             }
             *c = i + expanded;
         }
@@ -46,14 +46,18 @@ impl Universe {
 
 pub fn run(input: &str) -> Result<(u64, u64)> {
     let universe = parse(input)?;
-    let universe = universe.expand();
+    Ok((solve(&universe, 2), solve(&universe, 1_000_000)))
+}
+
+fn solve(universe: &Universe, expansion: usize) -> u64 {
+    let universe = universe.expand(expansion);
     let mut sum: u64 = 0;
     for g1 in &universe.galaxies {
         for g2 in &universe.galaxies {
             sum += manhattan(g1, g2) as u64;
         }
     }
-    Ok((sum / 2, 0))
+    sum / 2
 }
 
 fn manhattan(p1: &Point, p2: &Point) -> u32 {
@@ -82,5 +86,16 @@ mod tests {
     use crate::runner::{test_input, test_sample};
 
     test_sample!(sample_part1, 11, Some(374), None);
+    test_sample!(sample_part2, 11, None, Some(82000210));
     test_input!(part1, 11, Some(9329143), None);
+    test_input!(part2, 11, None, Some(710674907809));
+
+    #[test]
+    fn sample_part2_at_different_scales() -> color_eyre::Result<()> {
+        let input = std::fs::read_to_string("inputs/11/sample.txt")?;
+        let universe = parse(&input)?;
+        assert_eq!(1030, solve(&universe, 10));
+        assert_eq!(8410, solve(&universe, 100));
+        Ok(())
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -33,7 +33,7 @@ const DAYS: [fn() -> color_eyre::Result<()>; 11] = [
     || normal_day(day8::run, 8, 15517, 14935034899483),
     || normal_day(day9::run, 9, 1819125966, 1140),
     || normal_day(day10::run, 10, 6923, 529),
-    || normal_day(day11::run, 11, 9329143, 0),
+    || normal_day(day11::run, 11, 9329143, 710674907809),
 ];
 
 fn main() -> color_eyre::Result<()> {
