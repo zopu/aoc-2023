b3da54c5b03093b463c3786b7cd8c82acb3e04f3
Mike Perrow <mike.perrow@gmail.com>
2023-12-12 07:26:26 +0000

    day 12: scaffold

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,7 @@ mod runner;
 mod day1;
 mod day10;
 mod day11;
+mod day12;
 mod day2;
 mod day3;
 mod day4;
@@ -24,7 +25,7 @@ struct AppArgs {
     parallel: bool,
 }
 
-const DAYS: [fn() -> color_eyre::Result<()>; 11] = [
+const DAYS: [fn() -> color_eyre::Result<()>; 12] = [
     || normal_day(day1::run, 1, 55816, 54980),
     || normal_day(day2::run, 2, 2685, 83707),
     || normal_day(day3::run, 3, 527369, 73074886),
@@ -36,6 +37,7 @@ const DAYS: [fn() -> color_eyre::Result<()>; 11] = [
     || normal_day(day9::run, 9, 1819125966, 1140),
     || normal_day(day10::run, 10, 6923, 529),
     || normal_day(day11::run, 11, 9329143, 710674907809),
+    || normal_day(day12::run, 12, 0, 0),
 ];
 
 fn main() -> color_eyre::Result<()> {
