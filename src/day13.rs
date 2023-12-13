0ce852dd36111337c7f51b5ba7fbce1f6ec56ff0
Mike Perrow <mike.perrow@gmail.com>
2023-12-13 16:22:54 +0000

    day 13: perf: +~7% by using fixed-size arrays for the grid

--- src/day13.rs
+++ src/day13.rs
@@ -3,14 +3,16 @@ use rayon::iter::{ParallelBridge, ParallelIterator};
 
 #[derive(Debug)]
 struct Grid {
-    rows: Vec<u64>,
-    cols: Vec<u64>,
+    dimensions: (usize, usize),
+    rows: [u32; 20],
+    cols: [u32; 20],
 }
 
 impl From<&str> for Grid {
     fn from(input: &str) -> Self {
-        let mut rows = Vec::new();
-        let mut cols = vec![0; input.lines().next().unwrap().len()];
+        let (mut rows, mut cols) = ([0; 20], [0; 20]);
+        let mut row_count = 0;
+        let mut col_count = 0;
         for (l_i, line) in input.lines().enumerate() {
             let mut row = 0;
             for (i, c) in line.chars().enumerate() {
@@ -18,55 +20,63 @@ impl From<&str> for Grid {
                     row |= 1 << i;
                     cols[i] |= 1 << l_i;
                 }
+                col_count += 1;
             }
-            rows.push(row);
+            rows[l_i] = row;
+            row_count += 1;
         }
-        Self { rows, cols }
+        let dimensions = (row_count, col_count / row_count);
+        Self {
+            dimensions,
+            rows,
+            cols,
+        }
     }
 }
 
 pub fn run(input: &str) -> Result<(u64, u64)> {
-    let (p1, p2): (u64, u64) = input
+    let (p1, p2): (u32, u32) = input
         .split("\n\n")
         .par_bridge()
         .map(Grid::from)
         .map(|g| {
-            let row_symmetry = find_symmetry(&g.rows);
-            let col_symmetry = find_symmetry(&g.cols);
-            let oo_row_symmetry = find_one_off_symmetry(&g.rows);
-            let oo_col_symmetry = find_one_off_symmetry(&g.cols);
+            // println!("{:?}", g);
+            let row_symmetry = find_symmetry(&g.rows[0..g.dimensions.0]);
+            let col_symmetry = find_symmetry(&g.cols[0..g.dimensions.1]);
+            let oo_row_symmetry = find_one_off_symmetry(&g.rows[0..g.dimensions.0]);
+            let oo_col_symmetry = find_one_off_symmetry(&g.cols[0..g.dimensions.1]);
             (
                 col_symmetry + 100 * row_symmetry,
                 oo_col_symmetry + 100 * oo_row_symmetry,
             )
         })
         .reduce(|| (0, 0), |(a1, a2), (b1, b2)| (a1 + b1, a2 + b2));
-    Ok((p1, p2))
+    Ok((p1 as u64, p2 as u64))
 }
 
-fn find_symmetry(nums: &[u64]) -> u64 {
+fn find_symmetry(nums: &[u32]) -> u32 {
     'outer: for i in 1..nums.len() {
         // Check if there is a symmetry line before row i
         for j in 0..=i {
             if i + j > nums.len() {
-                return i as u64;
+                return i as u32;
             }
             if nums[i + j - 1] != nums[i - j] {
                 continue 'outer;
             }
         }
-        return i as u64;
+        return i as u32;
     }
     0
 }
 
-fn find_one_off_symmetry(nums: &[u64]) -> u64 {
+fn find_one_off_symmetry(nums: &[u32]) -> u32 {
     'outer: for i in 1..nums.len() {
         let mut bit_diff = 0;
         for j in 1..=i {
             if i + j > nums.len() {
                 if bit_diff == 1 {
-                    return i as u64;
+                    return i as u32;
                 } else {
                     continue 'outer;
                 }
@@ -77,7 +87,7 @@ fn find_one_off_symmetry(nums: &[u64]) -> u64 {
             }
         }
         if bit_diff == 1 {
-            return i as u64;
+            return i as u32;
         }
     }
     0
