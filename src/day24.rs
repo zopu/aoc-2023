199039bbedcd07e09b901a16a7a2a00cfcb9b0ab
Mike Perrow <mike.perrow@gmail.com>
2023-12-28 11:48:20 +0800

    day 24: perf: 2000x speedup doing it geometrically vs using z3. Now runs in under 1ms

--- src/day24.rs
+++ src/day24.rs
@@ -4,25 +4,24 @@ use nom::bytes::complete::tag;
 use nom::character::complete::{i128 as ni128, multispace0};
 use nom::sequence::tuple;
 use nom::IResult;
-use z3::ast::Ast;
 
 #[derive(Debug, PartialEq)]
 struct Line {
     at: (f64, f64, f64),
-    dir: (f64, f64, f64),
+    dir: (i16, i16, i16),
 }
 
 impl Line {
-    fn new(at: (f64, f64, f64), dir: (f64, f64, f64)) -> Self {
+    fn new(at: (f64, f64, f64), dir: (i16, i16, i16)) -> Self {
         Self { at, dir }
     }
 
     fn slope_and_offset2d(&self) -> (f64, f64) {
         let (x1, y1, _z1) = self.at;
         let (x2, y2, _z2) = (
-            (self.at.0 + self.dir.0),
-            (self.at.1 + self.dir.1),
-            (self.at.2 + self.dir.2),
+            (self.at.0 + self.dir.0 as f64),
+            (self.at.1 + self.dir.1 as f64),
+            (self.at.2 + self.dir.2 as f64),
         );
         let a = (y2 - y1) / (x2 - x1);
         let b = y1 - a * x1;
@@ -42,150 +41,51 @@ impl Line {
         let p_y = a * p_x + c;
 
         if !self.point_in_future(p_x, p_y) {
-            // println!("Intersection in past between {:?} and {:?}", self, other);
             return None;
         }
         if !other.point_in_future(p_x, p_y) {
-            // println!("Intersection in past between {:?} and {:?}", self, other);
             return None;
         }
 
         Some((p_x, p_y))
     }
 
+    // We have some numerical instability to deal with, so we only check approximate intersection
+    fn approx_intersects(&self, other: &Line) -> bool {
+        if let Some((ix, _iy)) = self.intersects2d(other) {
+            let (x1, _y1, z1) = self.at;
+            let (dx, _dy, dz) = self.dir;
+            let t = (ix - x1) / dx as f64;
+            let iz = z1 + t * dz as f64;
+            let z2 = other.at.2 + t * other.dir.2 as f64;
+            if (iz - z2).abs() > 100.0 {
+                println!("z1: {}, z2: {}, iz: {}", z1, z2, iz);
+                return false;
+            }
+            true
+        } else {
+            println!("No intersection");
+            false
+        }
+    }
+
     fn point_in_future(&self, x2: f64, y2: f64) -> bool {
         let (x1, y1, _z1) = self.at;
         let (dx, dy, _dz) = self.dir;
-        if dx > 0.0 && x2 < x1 {
+        if dx > 0 && x2 < x1 {
             return false;
         }
-        if dx < 0.0 && x2 > x1 {
+        if dx < 0 && x2 > x1 {
             return false;
         }
-        if dy > 0.0 && y2 < y1 {
+        if dy > 0 && y2 < y1 {
             return false;
         }
-        if dy < 0.0 && y2 > y1 {
+        if dy < 0 && y2 > y1 {
             return false;
         }
         true
     }
-
-    fn normalize(&self) -> Line {
-        let (dx, dy, dz) = self.dir;
-        let len = (dx * dx + dy * dy + dz * dz).sqrt();
-        Line::new(self.at, (dx / len, dy / len, dz / len))
-    }
-}
-
-pub fn run(input: &str) -> Result<(u64, u64)> {
-    let p1 = part1(input, 200_000_000_000_000.0, 400_000_000_000_000.0)?;
-    // let p2 = part2(input)?;
-    let p2 = part2_z3(input)?;
-    Ok((p1, p2))
-}
-
-#[allow(unused)]
-fn part2_geo(input: &str) -> Result<u64> {
-    let lines: Vec<_> = input
-        .lines()
-        .map(|l| Ok(parse_line(l).map_err(|e| anyhow!("Parse error: {e}"))?.1))
-        .collect::<Result<Vec<_>>>()?;
-
-    // For each pair of lines, we can find their intersection and the plane that
-    // contains both of them.
-
-    // Our rock's path must either lie on this plane or intersect it at their intersection point
-
-    // Find the plane containing lines 1 and 2
-    todo!("This doesn't work because these lines don't intersect");
-    println!("Lines: {:?}", lines);
-    let plane1_normal = normalize(cross_product(
-        lines[0].normalize().dir,
-        lines[1].normalize().dir,
-    ));
-
-    // Find the plane containing lines 3 and 4
-    let plane2_normal = normalize(cross_product(
-        lines[2].normalize().dir,
-        lines[3].normalize().dir,
-    ));
-
-    println!("Plane 1 normal: {:?}", plane1_normal);
-    println!("Plane 2 normal: {:?}", plane2_normal);
-
-    // Find an intersection point b/w plane 1 and line 3
-    let intersection_point =
-        line_plane_intersection(&lines[3].normalize(), lines[0].at, plane1_normal);
-    println!("Intersection point: {:?}", intersection_point);
-
-    // Find the intersection of these two planes, which is a line
-    let intersection_dir = cross_product(plane1_normal, plane2_normal);
-
-    println!("Intersection line dir: {:?}", intersection_dir);
-
-    let test_line = Line::new((24.0, 13.0, 10.0), (-3.0, 1.0, 2.0));
-    println!(
-        "test: {:?}",
-        line_plane_intersection(&test_line, lines[0].at, plane1_normal)
-    );
-
-    println!(
-        "test: {:?}",
-        line_plane_intersection(&test_line, lines[1].at, plane1_normal)
-    );
-
-    // Find the line's time at intersection with line 5
-    // Work out where this line is at time 0
-
-    Ok(0)
-}
-
-fn part2_z3(input: &str) -> Result<u64> {
-    let lines: Vec<_> = input
-        .lines()
-        .map(|l| Ok(parse_line(l).map_err(|e| anyhow!("Parse error: {e}"))?.1))
-        .collect::<Result<Vec<_>>>()?;
-
-    let cfg = z3::Config::new();
-    let ctx = z3::Context::new(&cfg);
-    let solver = z3::Solver::new(&ctx);
-
-    let x = z3::ast::Int::new_const(&ctx, "x");
-    let y = z3::ast::Int::new_const(&ctx, "y");
-    let z = z3::ast::Int::new_const(&ctx, "z");
-    let dx = z3::ast::Int::new_const(&ctx, "dx");
-    let dy = z3::ast::Int::new_const(&ctx, "dy");
-    let dz = z3::ast::Int::new_const(&ctx, "dz");
-
-    for hailstone in lines {
-        let hs_x = z3::ast::Int::from_i64(&ctx, hailstone.at.0 as i64);
-        let hs_y = z3::ast::Int::from_i64(&ctx, hailstone.at.1 as i64);
-        let hs_z = z3::ast::Int::from_i64(&ctx, hailstone.at.2 as i64);
-        let hs_dx = z3::ast::Int::from_i64(&ctx, hailstone.dir.0 as i64);
-        let hs_dy = z3::ast::Int::from_i64(&ctx, hailstone.dir.1 as i64);
-        let hs_dz = z3::ast::Int::from_i64(&ctx, hailstone.dir.2 as i64);
-        let t_n = z3::ast::Int::fresh_const(&ctx, "t");
-
-        solver.assert(&(&hs_x + &hs_dx * &t_n)._eq(&(&x + &dx * &t_n)));
-        solver.assert(&(&hs_y + &hs_dy * &t_n)._eq(&(&y + &dy * &t_n)));
-        solver.assert(&(&hs_z + &hs_dz * &t_n)._eq(&(&z + &dz * &t_n)));
-    }
-
-    solver.check();
-    let model = solver.get_model().unwrap();
-    let x = model.get_const_interp(&x).unwrap().as_i64().unwrap();
-    let y = model.get_const_interp(&y).unwrap().as_i64().unwrap();
-    let z = model.get_const_interp(&z).unwrap().as_i64().unwrap();
-
-    // 6728171924899227280 too high
-    Ok((x + y + z) as u64)
-}
-
-fn normalize(dir: (f64, f64, f64)) -> (f64, f64, f64) {
-    let (dx, dy, dz) = dir;
-    let len = (dx * dx + dy * dy + dz * dz).sqrt();
-    (dx / len, dy / len, dz / len)
 }
 
 fn line_plane_intersection(
@@ -204,15 +104,16 @@ fn line_plane_intersection(
         + line_origin_to_plane_point.1 * plane_normal.1
         + line_origin_to_plane_point.2 * plane_normal.2;
 
-    let line_direction_dot_plane_normal =
-        line.dir.0 * plane_normal.0 + line.dir.1 * plane_normal.1 + line.dir.2 * plane_normal.2;
+    let line_direction_dot_plane_normal = line.dir.0 as f64 * plane_normal.0
+        + line.dir.1 as f64 * plane_normal.1
+        + line.dir.2 as f64 * plane_normal.2;
 
     let t = -dot_product / line_direction_dot_plane_normal;
 
     (
-        line.at.0 + t * line.dir.0,
-        line.at.1 + t * line.dir.1,
-        line.at.2 + t * line.dir.2,
+        line.at.0 + t * line.dir.0 as f64,
+        line.at.1 + t * line.dir.1 as f64,
+        line.at.2 + t * line.dir.2 as f64,
     )
 }
 
@@ -222,6 +123,112 @@ fn cross_product(dir1: (f64, f64, f64), dir2: (f64, f64, f64)) -> (f64, f64, f64
         dir1.2 * dir2.0 - dir1.0 * dir2.2,
         dir1.0 * dir2.1 - dir1.1 * dir2.0,
     )
+}
+
+pub fn run(input: &str) -> Result<(u64, u64)> {
+    let p1 = part1(input, 200_000_000_000_000.0, 400_000_000_000_000.0)?;
+    // let p2 = part2(input)?;
+    let p2 = part2_faster(input)?;
+    Ok((p1, p2))
+}
+
+fn part2_faster(input: &str) -> Result<u64> {
+    let mut lines: Vec<_> = input
+        .lines()
+        .map(|l| Ok(parse_line(l).map_err(|e| anyhow!("Parse error: {e}"))?.1))
+        .collect::<Result<Vec<_>>>()?;
+
+    lines.sort_by(|a, b| a.dir.0.partial_cmp(&b.dir.0).unwrap());
+
+    let pairs_same_dx: Vec<_> = lines
+        .windows(2)
+        .filter(|lines| lines[0].dir.0 == lines[1].dir.0)
+        .map(|lines| (lines[0].at.0, lines[1].at.0, lines[0].dir.0))
+        .collect();
+
+    let dx_candidates = (-500..500)
+        .filter(|n| {
+            *n != 0
+                && pairs_same_dx.iter().all(|(a, b, dx)| {
+                    (n - dx != 0) && (*a as i64 - *b as i64).abs() % (n - dx).abs() as i64 == 0
+                })
+        })
+        .collect::<Vec<_>>();
+
+    lines.sort_by(|a, b| a.dir.1.partial_cmp(&b.dir.1).unwrap());
+
+    let pairs_same_dy: Vec<_> = lines
+        .windows(2)
+        .filter(|lines| lines[0].dir.1 == lines[1].dir.1)
+        .map(|lines| (lines[0].at.1, lines[1].at.1, lines[0].dir.1))
+        .collect();
+
+    let dy_candidates = (-500..500)
+        .filter(|n| {
+            *n != 0
+                && pairs_same_dy.iter().all(|(a, b, dy)| {
+                    (n - dy != 0) && (*a as i64 - *b as i64).abs() % (n - dy).abs() as i64 == 0
+                })
+        })
+        .collect::<Vec<_>>();
+
+    lines.sort_by(|a, b| a.dir.2.partial_cmp(&b.dir.2).unwrap());
+
+    let pairs_same_dz: Vec<_> = lines
+        .windows(2)
+        .filter(|lines| lines[0].dir.2 == lines[1].dir.2)
+        .map(|lines| (lines[0].at.2, lines[1].at.2, lines[0].dir.2))
+        .collect();
+
+    let dz_candidates = (-500..500)
+        .filter(|n| {
+            *n != 0
+                && pairs_same_dz.iter().all(|(a, b, dz)| {
+                    (n - dz != 0) && (*a as i64 - *b as i64).abs() % (n - dz).abs() as i64 == 0
+                })
+        })
+        .collect::<Vec<_>>();
+
+    // For every set of candidates, let's assume it's correct and check if it matches the other hailstones
+    // NB this looks like an n^3, but in practice with the real input each candidate set has only one element
+    for dx in &dx_candidates {
+        for dy in &dy_candidates {
+            for dz in &dz_candidates {
+                if let Some((x, y, z)) = check_dxyz(&lines, (*dx, *dy, *dz)) {
+                    return Ok((x as u64) + (y as u64) + z as u64);
+                }
+            }
+        }
+    }
+    Ok(0)
+}
+
+fn check_dxyz(hailstones: &[Line], (dx, dy, dz): (i16, i16, i16)) -> Option<(f64, f64, f64)> {
+    let (hs0_dx, hs0_dy, hs0_dz) = hailstones[0].dir;
+    let plane_normal = cross_product(
+        (hs0_dx as f64, hs0_dy as f64, hs0_dz as f64),
+        (dx as f64, dy as f64, dz as f64),
+    );
+    let plane_point = hailstones[0].at;
+    let intersection = line_plane_intersection(&hailstones[1], plane_point, plane_normal);
+    if !is_int(intersection.0) || !is_int(intersection.1) || !is_int(intersection.2) {
+        return None;
+    }
+    let (ix, iy, iz) = intersection;
+    let t = ((ix - hailstones[1].at.0) / hailstones[1].dir.0 as f64).abs();
+    let candidate = (ix - t * dx as f64, iy - t * dy as f64, iz - t * dz as f64);
+    // Now we need to check the candidate against every hailstone
+    for hs in hailstones {
+        if !hs.approx_intersects(&Line::new(candidate, (dx, dy, dz))) {
+            return None;
+        }
+    }
+
+    Some(candidate)
+}
+
+fn is_int(x: f64) -> bool {
+    x.fract() == 0.0
 }
 
 fn part1(input: &str, min_xy: f64, max_xy: f64) -> Result<u64> {
@@ -283,7 +290,7 @@ fn parse_line(input: &str) -> IResult<&str, Line> {
         remaining,
         Line::new(
             (a as f64, b as f64, c as f64),
-            (d as f64, e as f64, f as f64),
+            (d as i16, e as i16, f as i16),
         ),
     ))
 }
@@ -293,7 +300,7 @@ mod tests {
     use super::*;
     use crate::runner::test::{input_test, sample_test};
 
-    sample_test!(sample_part2, 24, None, Some(0));
+    sample_test!(sample_part2, 24, None, Some(47));
     input_test!(part1, 24, Some(19523), None);
 
     #[test]
